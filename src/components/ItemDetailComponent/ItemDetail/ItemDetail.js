732d6b0ea0b7d78faf754dca15fed1db1be0766d
echevalier <estebanchevalier@gmail.com>
2022-04-12 14:58:31 -0300

    Fix index.js to version 18

--- src/components/ItemDetailComponent/ItemDetail/ItemDetail.js
+++ src/components/ItemDetailComponent/ItemDetail/ItemDetail.js
@@ -8,12 +8,12 @@ const ItemDetail = ({ product }) => {
     return (
         <Card className="my-5">
             <Row className="g-0">
-                <Col md={4} lg={{ span: 2, offset: 3 }}>
-                    <img id="img" className="card-img-top" src={"/assets/img/product/" + product.id + ".jpg"} alt={product.name} />
+                <Col md={4} lg={{ span: 3, offset: 1 }}>
+                    <img id="img" className="card-img-top" src={product.thumbnail} alt={product.title} />
                 </Col>
                 <Col md={8} lg={{ span: 4 }}>
                     <Card.Body className="text-center">
-                        <Card.Title className="fs-2">{product.name}</Card.Title>
+                        <Card.Title className="fs-2">{product.title}</Card.Title>
                         <Card.Text>Precio por Kg  <span className="fw-bold fs-4">${product.price}</span></Card.Text>
                         <Card.Text className="my-5 text-start">{product.description}</Card.Text>
                     </Card.Body>
