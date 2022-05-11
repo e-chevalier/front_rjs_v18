1a015fca6d3c1397c875c3e7496a793bb551045d
e-chevalier <echevalier.dev@gmail.com>
2022-05-11 01:53:29 -0300

    Add onSubmit redirect to another page

--- src/components/ItemComponent/ItemList/ItemList.js
+++ src/components/ItemComponent/ItemList/ItemList.js
@@ -2,8 +2,12 @@ import Item from '../Item/Item'
 import Row from 'react-bootstrap/Row'
 
 const ItemList = ({ products }) => {
+
+  console.log("Productos length: " + products.length)
+
   return (
-    <>
+    products.length > 0?
+    <>     
       <Row xs={1} sm={2} md={2} lg={3} className="g-4">
         {
           products.map(prod =>
@@ -11,8 +15,9 @@ const ItemList = ({ products }) => {
           )
         }
       </Row>
+    </>:
+    <p className="fs-3">Sin resultados.</p>
 
-    </>
   )
 }
 
